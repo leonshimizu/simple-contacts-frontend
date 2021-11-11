82aa596862dbb78704bf998c4e9514f26861ef6e
“leonshimizu” <lmshimizu@gmail.com>
2021-11-10 19:11:44 -0800

    contacts index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <!-- <h1>{{ contacts }}</h1> -->
+    <ul>
+      <li v-for="contact in contacts">
+       {{ contact.id }}
+       {{ contact.first_name }} {{ contact.last_name }}
+       {{ contact.email }}
+       {{ contact.phone_number }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
@@ -15,7 +24,22 @@ export default {
   },
   data: function() {
     return {
-      message: "Hello World"
+      message: "Hello World",
+      contacts: []
+    }
+  },
+  created: function() {
+    this.indexFunction();
+  },
+  methods: {
+    indexFunction: function() {
+      console.log("in the index function");
+      axios
+        .get('http://localhost:3000/contacts')
+        .then(response => {
+          console.log(response.data);
+          this.contacts = response.data;
+        })
     }
   }
 };
