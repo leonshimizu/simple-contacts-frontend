0c8cc98107022644af5c42964d55a11d1c2e5168
“leonshimizu” <lmshimizu@gmail.com>
2021-11-10 19:20:40 -0800

    added create function - started show function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
     <!-- <h1>{{ contacts }}</h1> -->
+    <h1>Here are all the contacts:</h1>
+    <button v-on:click="createFunction()">Add Contact</button>
     <ul>
       <li v-for="contact in contacts">
        {{ contact.id }}
        {{ contact.first_name }} {{ contact.last_name }}
        {{ contact.email }}
        {{ contact.phone_number }}
+       <button v-on:click="showFunction()">Show More Info</button>
       </li>
     </ul>
   </div>
@@ -40,6 +43,23 @@ export default {
           console.log(response.data);
           this.contacts = response.data;
         })
+    },
+    createFunction: function() {
+      console.log("in the create function");
+      axios 
+        .post('http://localhost:3000/contacts.json', {
+          first_name: "Stassie",
+          last_name: "Shimizu",
+          email: "stassie@gmail.com",
+          phone_number: "0987654321"
+        }).then(response => {
+          console.log(response.data);
+          this.contacts.push(response.data);
+        })
+    },
+    showFunction: function() {
+      console.log("in the show function");
+      // document.querySelector
     }
   }
 };
