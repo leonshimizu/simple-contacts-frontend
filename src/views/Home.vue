d9f20eea4e22fdd14c2e01ac2138b2ffde7b7bc9
“leonshimizu” <lmshimizu@gmail.com>
2021-11-10 18:56:04 -0800

    started vue app - added message - hello world

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
@@ -13,6 +12,11 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      message: "Hello World"
+    }
   }
-}
+};
 </script>
